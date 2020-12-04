b8b8febc92cc9bc73ee8d2940bce373de9471d38
Irina <irina.bota@ppb.com>
2020-12-04 16:15:16 +0200

    improvements

--- day4/day4-2.py
+++ day4/day4-2.py
@@ -21,32 +21,16 @@ required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 count = 0
 for pasp in passports:
     if all(key in pasp for key in required_fields):
-        print(pasp)
-        if int(pasp["byr"]) in range(1920, 2002+1) and int(pasp["iyr"]) in range(2010, 2020+1) and \
-                int(pasp["eyr"]) in range(2020, 2030+1):
-            pass
-        else:
-            continue
-        if "cm" in pasp["hgt"] and int(pasp["hgt"].split("cm")[0]) in range(150, 193 + 1):
-            pass
-        elif "in" in pasp["hgt"] and int(pasp["hgt"].split("in")[0]) in range(59, 76 + 1):
-            pass
-        else:
-            continue
-        if pasp["hcl"][0] == "#" and all(i.isalpha() or i.isdigit() for i in pasp["hcl"].split("#")[1]):
-            pass
-        else:
-            continue
-        if pasp["ecl"] in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth"):
-            pass
-        else:
-            continue
-        if len(pasp["pid"]) == 9 and all(i.isdigit for i in pasp["pid"]):
-            pass
-        else:
-            continue
-        count += 1
+        conds = []
+        conds.append(int(pasp["byr"]) in range(1920, 2002+1) and int(pasp["iyr"]) in range(2010, 2020+1) and \
+                int(pasp["eyr"]) in range(2020, 2030+1))
+        conds.append( ("cm" in pasp["hgt"] and int(pasp["hgt"].split("cm")[0]) in range(150, 193 + 1)) or \
+                ("in" in pasp["hgt"] and int(pasp["hgt"].split("in")[0]) in range(59, 76 + 1)))
+        conds.append(pasp["hcl"][0] == "#" and all(i.isalpha() or i.isdigit() for i in pasp["hcl"].split("#")[1]))
+        conds.append(pasp["ecl"] in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth"))
+        conds.append(len(pasp["pid"]) == 9 and all(i.isdigit for i in pasp["pid"]))
 
-
+        if all(x for x in conds):
+            count += 1
 
 print(count)
